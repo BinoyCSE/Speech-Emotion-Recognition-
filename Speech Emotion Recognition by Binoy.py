f2c9cdfe19ec3f8891c878a366f14d5b101b0a90
Binoy Das Gupta <50591958+BinoyCSE@users.noreply.github.com>
2024-03-17 00:13:26 +0600

    Update Speech Emotion Recognition by Binoy.py

--- Speech Emotion Recognition by Binoy.py
+++ Speech Emotion Recognition by Binoy.py
@@ -1,20 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 pip install pydub
 
-
-# In[2]:
-
-
 from pydub import AudioSegment
-
-
-# In[3]:
-
 
 import numpy as np
 import pandas as pad
@@ -23,19 +9,11 @@ import os
 import glob
 import sys
 
-
-# In[4]:
-
-
 import librosa
 import librosa.display
 import seaborn as sbn
 import matplotlib.pyplot as mplt
 from sklearn import metrics
-
-
-# In[5]:
-
 
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.metrics import confusion_matrix, classification_report
@@ -43,30 +21,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
-
-# In[6]:
-
-
 from IPython.display import Audio
-
-
-# In[7]:
-
 
 import warnings
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-
-# In[8]:
-
-
+#Dataset 
 RavdessData = "D:\\CUET\\Level-4, Term-2\\Thesis works\\ravdess data\\audio_speech_actors_01-24\\"
-
-
-# In[9]:
-
 
 ravdessDirectoryList = os.listdir(RavdessData)
 fileEmotion = []
@@ -82,24 +45,13 @@ emotion_df = pad.DataFrame(fileEmotion, columns=['Emotions'])
 path_df = pad.DataFrame(filePath, columns=['Path'])
 Ravdess_df = pad.concat([emotion_df, path_df], axis=1)
 
-
-# In[10]:
-
-
 Ravdess_df.Emotions.replace({1:'neutral', 2:'calm', 3:'happy', 4:'sad', 5:'angry', 6:'fear',7: 'disgust', 8: 'surprise'}, inplace=True)
 Ravdess_df.head
-
-
-# In[11]:
-
 
 dataPath = Ravdess_df.copy()
 dataPath.head
 
-
-# In[12]:
-
-
+#emotion plotting based on dataset
 mplt.title('Count of Emotions', size=16)
 sbn.countplot(data=dataPath, x='Emotions')
 mplt.ylabel('Count', size=12)
@@ -107,19 +59,11 @@ mplt.xlabel('Emotions', size=12)
 sbn.despine(top=True, right=True, left=False, bottom=False)
 mplt.show()
 
-
-# In[13]:
-
-
 def createWaveplot(data, sr, e):
     mplt.figure(figsize=(10, 3))
     mplt.title('Waveplot for audio with {} emotion'.format(e),size=15)
     librosa.display.waveshow(data, sr=sr)
     mplt.show()
-
-
-# In[14]:
-
 
 def createSpectrogram(data, sr, e):
     X = librosa.stft(data)
@@ -129,20 +73,12 @@ def createSpectrogram(data, sr, e):
     librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
     mplt.colorbar()
 
-
-# In[15]:
-
-
 emotion='fear'
 path = np.array(dataPath.Path[dataPath.Emotions==emotion])[1]
 data, samplingRate = librosa.load(path)
 createWaveplot(data, samplingRate, emotion)
 createSpectrogram(data, samplingRate, emotion)
 Audio(path)
-
-
-# In[16]:
-
 
 emotion='angry'
 path = np.array(dataPath.Path[dataPath.Emotions==emotion])[1]
@@ -151,20 +87,12 @@ createWaveplot(data, samplingRate, emotion)
 createSpectrogram(data, samplingRate, emotion)
 Audio(path)
 
-
-# In[17]:
-
-
 emotion='happy'
 path = np.array(dataPath.Path[dataPath.Emotions==emotion])[1]
 data, samplingRate = librosa.load(path)
 createWaveplot(data, samplingRate, emotion)
 createSpectrogram(data, samplingRate, emotion)
 Audio(path)
-
-
-# In[53]:
-
 
 def noise(data):
     noiseAmp = 0.035*np.random.uniform()*np.amax(data)
@@ -186,52 +114,35 @@ def pitch(data, pitchFactor=0.7):
     return librosa.effects.pitch_shift(data, sr=sampleRate, n_steps=int(abs(pitchFactor) * len(data) / sampleRate))
 
 
-# In[54]:
-
-
 path = np.array(dataPath.Path)[1]
 data, sampleRate = librosa.load(path)
 
-
-# In[55]:
-
-
+#noise 
 x = noise(data)
 mplt.figure(figsize=(14,4))
 librosa.display.waveshow(y=x, sr=sampleRate)
 Audio(x, rate=sampleRate)
 
-
-# In[56]:
-
-
+#stretch
 x = stretch(data)
 mplt.figure(figsize=(14,4))
 librosa.display.waveshow(y=x, sr=sampleRate)
 Audio(x, rate=sampleRate)
 
-
-# In[57]:
-
-
+#shift
 x = shift(data)
 mplt.figure(figsize=(14,4))
 librosa.display.waveshow(y=x, sr=sampleRate)
 Audio(x, rate=sampleRate)
 
-
-# In[59]:
-
-
+#pitch
 x = pitch(data)
 mplt.figure(figsize=(14,4))
 librosa.display.waveshow(y=x, sr=sampleRate)
 Audio(x, rate=sampleRate)
 
 
-# In[70]:
-
-
+#audio feature extraction
 def extract_feature(file_name, mfcc, chroma, mel):
     with soundfile.SoundFile(file_name) as sound_file:
         X = sound_file.read(dtype="float32")
@@ -251,9 +162,6 @@ def extract_feature(file_name, mfcc, chroma, mel):
     return result
 
 
-# In[71]:
-
-
 emotions={
   '01':'neutral',
   '02':'calm',
@@ -268,10 +176,6 @@ emotions={
 #DataFlair - Emotions to observe
 observed_emotions=['calm', 'happy', 'fear', 'disgust']
 
-
-# In[72]:
-
-
 def load_data(test_size=0.2):
     x,y=[],[]
     for file in glob.glob("D:\\CUET\\Level-4, Term-2\\Thesis works\\ravdess data\\RAVDESS\\Actor_*\\*.wav"):
@@ -285,35 +189,20 @@ def load_data(test_size=0.2):
     return train_test_split(np.array(x), y, test_size=test_size, random_state=9)
 
 
-# In[73]:
-
-
 #Splitting dataset
 xTrain,xTest,yTrain,yTest=load_data(test_size=0.23)
-
-
-# In[74]:
 
 
 #Getting number of features extracted
 print(f'Features extracted: {xTrain.shape[1]}')
 
 
-# In[75]:
-
-
 #Initializing the Multilayer-Perceptron Classifier
 model=MLPClassifier(alpha=0.01, batch_size=256, epsilon=1e-08, hidden_layer_sizes=(300,), learning_rate='adaptive', max_iter=500)
 
 
-# In[76]:
-
-
 #Training the model
 model.fit(xTrain,yTrain)
-
-
-# In[77]:
 
 
 #Prediciting for test set
@@ -321,20 +210,8 @@ expected_Of_y = yTest
 yPred=model.predict(xTest)
 
 
-# In[78]:
-
-
 print(metrics.confusion_matrix(expected_Of_y, yPred))
-
-
-# In[79]:
-
-
 print(classification_report(yTest, yPred))
-
-
-# In[80]:
-
 
 #Calculating accuracy of model
 accuracy=accuracy_score(y_true=yTest, y_pred=yPred)
